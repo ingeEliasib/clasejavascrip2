711b64364de35f649c26be764c814e60fd6aed01
eliasib <ingenieroeliasibcadena@gmail.com>
2023-11-20 15:09:43 -0500

    CRACION Y IMPLEMENTACION

---
+++ jscodigo.js
@@ -0,0 +1,49 @@
+alert("bienvenido")
+
+
+// let sexo="";
+// let primeraLetraNombre='f';
+// let nombreCompleto="";
+// let mensaje="";
+// let aprobado = false;
+
+// mensaje= "bienvenido a su programa de division de grupos";
+// mensaje += "\n por favor ingrese su nombre y sexo para la clasificacion";
+// nombreCompleto=prompt(mensaje+" ingrese su nombre").toLowerCase();
+// sexo= prompt("ingrese su sexo femenino o masculino").toLocaleLowerCase();
+
+// do{
+//     if(sexo=="masculino" || sexo=="femenino"){
+//         //seleccciono la letra ya tengo el sexo 
+//         primeraLetraNombre = nombreCompleto[0];
+//         //realizo el if
+//         if(primeraLetraNombre < "m"){
+//             alert("la letra $primeraLetraNombre es menor que m")
+//             if(sexo=="masculino"){
+//                 alert("su grupo es A")
+//             }else{
+
+//             }
+             
+//         }
+//         //salida del bucle
+//         aprobado= true;
+//     }else{
+//         alert("ingreso un sexo incorrecto o mal escrito \n ingrese femenino o masculino");
+//         sexo= prompt("ingrese su sexo femenino o masculino").toLocaleLowerCase();
+//         aprobado= false;
+//     }
+// }while(aprobado== true)
+
+let valorUsuario, calculo =0
+let total=0.0;
+
+valorUsuario= +prompt("ingrese el valor de declaracion de renta")
+
+if (valorUsuario=) {
+    
+} else {
+    
+}
+
+
