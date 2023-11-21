85827d971ad9c3d2c94f33fdd7bfafc1e929a178
eliasib <ingenieroeliasibcadena@gmail.com>
2023-11-21 12:59:10 -0500

    actualizacion switch

--- buclefor.js
+++ buclefor.js
@@ -1,3 +1,23 @@
 for (i=1; i<=5; i++) {
     document.write(i + "<br />");
  }
+
+ var i = 0;
+for (; i < 10; ) {
+    document.write(i);
+    i++;
+}
+// este caso se usa el for sin las declaraciones iniciales
+
+
+for (i=0; i<=20;i+=2 ) {
+    document.write(i);
+}
+// revisamos los pares hasta el numero 20
+
+var i=0;
+while (i<=10) {
+    document.write(i + "<br />");
+    i++;
+}
+// buble while
