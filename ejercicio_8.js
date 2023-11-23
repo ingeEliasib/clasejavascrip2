2c06a52e631836e01f96c0ce7a07c8c0dc413abd
eliasib <ingenieroeliasibcadena@gmail.com>
2023-11-23 14:12:20 -0500

    ejercicio 8

---
+++ ejercicio_8.js
@@ -0,0 +1,25 @@
+//ejercicio 8 por Ing Eliasib
+// variables
+
+let mensaje= `Bienvenido al software de calificacion de empleados de \n CoCacola S.A.` 
+let moneta=` €`;
+let variableUsuario;
+let total=0
+let a = 0.0;
+let b= 0.4;
+let c= 0.6;
+let d = 0.10;
+let e= 0.12;
+let f= 0.16;
+let arrayporusuario=[];
+let arrayOpcionesdeCalificacion = `el valor que usted deberà pagar es de :` 
+let  estadoSoftware=true;
+
+
+while (condition) {
+    
+}
+
+arregoejemplo[
+    ["jorge",200,"500 €",a,b,c],["daniel,",200,"500 €",a,c,c,d],["elias",200,"500 €",a,b,c,d]
+]
