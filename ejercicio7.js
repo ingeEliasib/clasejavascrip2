30af253d5a24e02353ae25264aa50b7958c04649
eliasib <ingenieroeliasibcadena@gmail.com>
2023-11-23 13:42:43 -0500

    ejercicio7 finalizado

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,39 +1,45 @@
+//declaracion de mis variables a utilizar
 
-let mensaje=`el valor del importe que ustede pagara es de `;
+let saludo=`bienvenido a su software tributario \n con este software vamos a revisar el porcentaje que le corresponde segun su declaración de renta  `;
+let mensaje= `el valor que usted deberà pagar es de :` 
 let moneda=` €`;
-let variableUsuario  = +prompt("ingrese el valor de su renta anual");
+let variableUsuario;
 let total=0
 let a = 0.05;
 let b= 0.15;
 let c= 0.20;
 let d= 0.30;
 let e = 0.45;
+let  estadoSoftware=true;
 
-if (variableUsuario>=60000) {
-    //revisamos cuando es mayor a 60 mil euros 
-    porcentaje(variableUsuario,e);
+//inicio mi interfaz de usuario
+
+alert(saludo);
+while (estadoSoftware) {
+    variableUsuario  = prompt("ingrese el valor de su renta anual ingrese x para salir");
+    if (variableUsuario!="x") {
+        variableUsuario= parseInt(variableUsuario)
+        if (variableUsuario>=60000) {
+            porcentaje(variableUsuario,e);
+        } else if (variableUsuario>=35000 && variableUsuario <60000) {
+            porcentaje(variableUsuario,d);
+        }else if (variableUsuario >=20000 && variableUsuario <35000) {
+            porcentaje(variableUsuario,c);
+        }else if ( variableUsuario>=20000 && variableUsuario <35000) {
+            porcentaje(variableUsuario,b);
+        }else if (variableUsuario>=20000 && variableUsuario<35000) {
+            porcentaje(variableUsuario,a);
+        }
+    }else {
+        alert("Esta saliendo del software que tenga buen dìa")
+        estadoSoftware=false
+    }
     
-} else if (variableUsuario>=35000 && variableUsuario<60000) {
+    }
 
-    porcentaje(variableUsuario,d);
-
-}else if (variableUsuario>=20000 && variableUsuario<35000) {
-
-    porcentaje(variableUsuario,c);
-
-}else if (variableUsuario>=20000 && variableUsuario<35000) {
-
-    porcentaje(variableUsuario,b);
-
-}else if (variableUsuario>=20000 && variableUsuario<35000) {
-
-    porcentaje(variableUsuario,a);
-
-}
 function porcentaje(x, y){
     total = x * y
     mensaje=(mensaje + total.toLocaleString().toString() + moneda);
-    //document.write("<H1>mensaje + total.toLocaleString().toString() + moneda</H1>")
     document.write("<H1>"+mensaje+"</H1>")
     return alert(mensaje + total.toLocaleString().toString() + moneda);
     
