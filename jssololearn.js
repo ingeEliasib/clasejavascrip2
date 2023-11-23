7ff07d0dab52fbae53f294a2fde8ac0b43c5a9ee
eliasib <ingenieroeliasibcadena@gmail.com>
2023-11-23 13:11:01 -0500

    pruebas de programacion

--- jssololearn.js
+++ jssololearn.js
@@ -1,7 +1,16 @@
 
-    var seconds = parseInt(prompt("ingrese numero"), 10)
+    // var seconds = parseInt(prompt("ingrese numero"), 10)
+    // // Tu código va aquí
+    // while(seconds >= 0){
+    //     seconds-=1
+    //     alert(seconds)
+    // }
+
+    let countOfRooms = 15;
+    let conteo = 0;
     // Tu código va aquí
-    while(seconds >= 0){
-        seconds-=1
-        alert(seconds)
+
+    while(countOfRooms<=15){
+        console.log(conteo+1);
+        conteo++;
     }
