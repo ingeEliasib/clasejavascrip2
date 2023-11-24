5cd7adfb97b4ad81ad67122d65d0cc1875be7b83
eliasib <ingenieroeliasibcadena@gmail.com>
2023-11-24 08:10:44 -0500

    EJERCICIO DIVISION MANUAL

--- jssololearn.js
+++ jssololearn.js
@@ -1,7 +1,23 @@
 
-    var seconds = parseInt(prompt("ingrese numero"), 10)
-    // Tu código va aquí
-    while(seconds >= 0){
-        seconds-=1
-        alert(seconds)
+    let mensaje =`Bienvenido a su software de division sin operador de dividendo \n `;
+    let numeroUno = parseInt(prompt(mensaje+`ingrese numero`), 10);
+    let numeroDos= parseInt(prompt("ingrese su numero Dos"),10);
+    let resultado,conteoCantidadRestasRealizadas=0;
+    //let continuarBucle = true;
+
+    DivisionManualAntigua(numeroUno,numeroDos);
+    function DivisionManualAntigua(x,y) {
+        while (x > y ) {
+            resultado= x - y;
+            conteoCantidadRestasRealizadas =conteoCantidadRestasRealizadas+1;
+        }
+        mensaje=`el resulado de realizar la division es ${conteoCantidadRestasRealizadas} \n el restante de la division es ${resultado}`;
+        alert(mensaje);
+        
     }
+    // Tu código va aquí
+    // while(seconds >= 0){
+    //     seconds-=1
+    //     alert(seconds)
+    // }
+    
