d7fa4780bb55673b677943c9a966c0acc9710137
eliasib <ingenieroeliasibcadena@gmail.com>
2023-11-24 16:50:28 -0500

    ejercicio division

--- jssololearn.js
+++ jssololearn.js
@@ -1,22 +1,45 @@
-
+    //creo mis variables
+    
     let mensaje =`Bienvenido a su software de division sin operador de dividendo \n `;
-    let numeroUno = parseInt(prompt(mensaje+`ingrese numero`), 10);
-    let numeroDos= parseInt(prompt("ingrese su numero Dos"),10);
-    let resultado,conteoCantidadRestasRealizadas=0;
-    //let continuarBucle = true;
+    let opciones=``;
+    let numeroUno , numeroDos,resultado,  conteoCantidadRestasRealizadas=0;
+    let repetir=true;
 
-    DivisionManualAntigua(numeroUno,numeroDos);
+    //realizamos el bucle
+    while (repetir) {
+        numeroUno = parseInt(prompt(mensaje+`ingrese numero`), 10);
+        numeroDos= +prompt("ingrese su numero Dos");
+        if (isNaN(numeroUno) || isNaN(numeroDos)) {
+            alert(`los valores que ingresen deben ser numericos`);
+        }else{
+            //reviso los numeros ingresados
+            if (numeroUno!=0 && numeroDos!=0 && numeroUno>numeroDos) {
+               //llamo a mi funcion de division
+                DivisionManualAntigua(numeroUno,numeroDos);
+                opciones=prompt(`si desea salir ingrese S  o C para continuar`).toLocaleLowerCase();
+                //reviso si el usuario quiere salir
+                
+                if (opciones == `s`) {
+                    repetir=false; 
+                } 
+            
+            } else {
+                alert(`los numeros que ingrese deben ser diferente de 0 y el numero uno debe ser mayor al segundo`);
+            }
+        }
+       
+    }
+    document.write("<h1>"+Gracias +"<\h1>");
+
+    //aqui creo la funcion
     function DivisionManualAntigua(x,y) {
         while (x > y ) {
             resultado= x - y;
             conteoCantidadRestasRealizadas =conteoCantidadRestasRealizadas+1;
+            x=resultado;
         }
         mensaje=`el resulado de realizar la division es ${conteoCantidadRestasRealizadas} \n el restante de la division es ${resultado}`;
         alert(mensaje);
         
     }
-    // Tu código va aquí
-    while(seconds >= 0){
-        seconds-=1
-        alert(seconds)
-    }
+    
