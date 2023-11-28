991381d58f47331110ab146fbdd4cc4f20b0ae34
eliasib <ingenieroeliasibcadena@gmail.com>
2023-11-28 14:39:10 -0500

    listo parrafo 22

--- trabajandoconparrafos.js
+++ trabajandoconparrafos.js
@@ -1,3 +1,22 @@
 //prueba con parrafos
 
-parrafo1
+
+parrafo1 = document.getElementById(parrafo1);
+console.log(parrafo1)
+
+
+
+alert("trabajando con el parrafo");
+
+document.write(parrafo1.remplace(".","</br></p> <p>"))
+//.indexOf(param) ***
+//Devuelve la posicion de la primera coincidencia partiendo desde la posicion 0, podemos asignarle varios caracteres y en caso de no haber coincidencia arroja -1
+// let msj = `Lorem ipsum amaretus`
+
+// console.log(msj.indexOf(`m`)); //4
+
+
+// // .replace(buscado,remplazador) ***
+// //El va a ubicar la primera coincidencia con el parametro "buscado" y si la consigue la va a remplazar por el "remplezados"
+
+// console.log(correo.replace("o","a")); //pepita@unc.com
